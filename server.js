8440a49d65c8938161e483c57b4a5d5f6d18b2b1
linhdhng <153128184+linhdhng@users.noreply.github.com>
2024-02-21 22:12:18 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,43 +1,39 @@
+const { error } = require("console");
+const express = require("express");
 
+const app = express();
 
-// Use at least three different data categories (e.g., users, posts, or comments).
-// Utilize reasonable data structuring practices.
-// Create GET routes for all data that should be exposed to the client.
-// Create POST routes for data, as appropriate. At least one data category should allow for client creation via a POST request.
-// Create PATCH or PUT routes for data, as appropriate. At least one data category should allow for client manipulation via a PATCH or PUT request.
-// Create DELETE routes for data, as appropriate. At least one data category should allow for client deletion via a DELETE request.
-// Include query parameters for data filtering, where appropriate. At least one data category should allow for additional filtering through the use of query parameters.
-// Utilize route parameters, where appropriate.
-// Adhere to the guiding principles of REST.
-// Create and render at least one view using a view template and template engine. This can be a custom template engine or a third-party engine.
-// If you are stuck on how to approach this, think about ways you could render the current state of your API's data for easy viewing.
-// Use simple CSS to style the rendered views.
-// Include a form within a rendered view that allows for interaction with your RESTful API.
-
-const express = require("express")
-const app = express()
-const bodyParser = require("body-parser")
 const port = 3000;
 
-const users = require()
+app.use(express.static('./styles'));
 
-// Create and use at least two pieces of custom middleware.
+const fs = require('fs');
 
-// Create and use error-handling middleware.
+app.engine('frontend', (filePath, data, callback) => {
+    fs.readFile(filePath, (error, content) => {
+        if (error) return callback (error);
 
-app.get("/api/users", (req, res) => {
-    res.json(users)
-})
+        const rendered = content
+            .toString()
+            .replaceAll("#Front#", `${data.word}`)
+            .replace("#Back#", `${data.translate}`);
+        return callback(null, rendered);
+    });
+});
 
-app.get("/", (req,res) => {
-    res.send("Work")
-})
+app.set('view', './view');
+app.set('view engine', 'frontend')
 
-// Custom middleware
-app.use((req,res) => {
-    res.status(404).json({ error: "Resource Not Found"})
-})
+app.get('/', (req, res) => {
+    const data = {
+        word: "привет",
+        translate: "hello",
+    };
+    res.render("index", data);
+});
 
+// express.static(root,[options]) //[req.param]
+//res.redirect('/redirect to a /location')
 app.listen(port, () => {
-    console.log(`Server listening on port: ${port}`)
-})
+    console.log(`Server listening on ${port}`);
+});

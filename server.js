4ba88e130193e51bb6757def31975a3b03fd7f1f
linhdhng <153128184+linhdhng@users.noreply.github.com>
2024-02-25 22:13:40 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 const express = require("express");
 const mongoose = require('mongoose');
-
+const card = require('./models/productModel');
 const db = require('./db/database');
 const app = express();
 
@@ -32,16 +32,41 @@ app.use(express.json());
 app.get('/', (req, res) => {
     const data = {
         word: "привет",
+        pronunciation: "privet",
         translate: "hello",
     };
 
     res.send(data);
 });
 
-app.post('/translate', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
+app.get('/data', async(req, res) => {
+    try {
+        const Card = await card.find({});
+        res.status(200).json(Card);
+    } catch (error) {
+        res.status(500).send('Not Found')
+    }
+})
+
+app.post('/translate', async(req, res) => {
+    try {
+        const Card = await card.create(req.body);
+        res.status(200).send(Card);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send('Not Found')
+    }
 });
+
+app.patch('translate/:id', async(req,res) => {
+    const {id} = req.params;
+    const Card = await Card.findByIdAndUpdate(id, req.body);
+    if(!Card) {
+        return res.status(400).send(`Cannot find any card with ${id}`);
+    }
+
+    res.status(200).json(Card);
+})
 
 // Error handling
 app.use((req, res) => {
@@ -50,7 +75,7 @@ app.use((req, res) => {
 app.use((err, req, res, next) => {
     res.status(500).send("Seems like we messed up somewhere...");
 });
-
+// Querry parameter to filter data
 // express.static(root,[options]) //[req.param]
 //res.redirect('/redirect to a /location')
 app.listen(port, () => {
